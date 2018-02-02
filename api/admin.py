992995f04c25b5b3827cbaa3c8ae12bf867123f7
prudywsh <antoineprudhomme5@gmail.com>
2018-02-02 11:36:30 +0100

    time zone set to FR + models created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Barcamp, Speaker, Admin, Talk
 
 # Register your models here.
+admin.site.register(Barcamp)
+admin.site.register(Speaker)
+admin.site.register(Admin)
+admin.site.register(Talk)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Barcamp(models.Model):
+    # fields
+    title = models.CharField(max_length=200, unique=True)
+    date = models.DateTimeField(unique=True)
+
+class Speaker(models.Model):
+    # fields
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+class Talk(models.Model):
+    # relations
+    barcamp = models.ForeignKey(Barcamp, on_delete=models.CASCADE)
+    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
+    # fields
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    slides_name = models.CharField(max_length=200, unique=True)
+
+class Admin(models.Model):
+    # fields
+    email = models.EmailField(unique=True)
