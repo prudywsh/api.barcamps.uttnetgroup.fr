ea1950858c515c16f0faf12f4b37b53c72e4aa14
prudywsh <antoineprudhomme5@gmail.com>
2018-03-14 18:59:57 +0100

    admin attribute added to JWT payload

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 from .models import Barcamp, Speaker, Talk
 from .serializers import BarcampSerializer, TalkSerializer, SpeakerSerializer
 from rest_framework import generics, status, permissions
@@ -57,10 +59,18 @@ class OauthToken(APIView):
         # get info about the user
         user_info = get_user_info(access_token)
 
+        # check if the user is admin
+        is_admin = True
+        try:
+            User.objects.get(email=user_info['email'])
+        except ObjectDoesNotExist:
+            is_admin = False
+
         user_jwt = jwt.encode({
             'email': user_info['email'],
             'firstName': user_info['firstName'],
             'lastName': user_info['lastName'],
+            'admin': is_admin
         }, os.environ.get('JWT_SECRET'), algorithm='HS256')
 
         return Response(user_jwt)
